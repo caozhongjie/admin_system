36fbb6833730664a1041e697772e668bcb7693e4
caozhongjie <848773542@qq.com>
2021-08-13 07:53:54 +0800

    静态

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs'); //文件模块
 
 /* GET home page. */
+const data= {
+  "success": true,
+  "cold": 200,
+  "data": [
+    {
+      "name": "张三12312312",
+      "age": "17",
+      "sex": 1,
+      "favorite": '足球'
+    },
+    {
+      "name": "张三",
+      "age": "17",
+      "sex": 1,
+      "favorite": '足球'
+    },
+    {
+      "name": "张三",
+      "age": "17",
+      "sex": 1,
+      "favorite": '足球'
+    }
+  ]
+}
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('frontIndex', { title: 'Express', ...data });
+
 });
 
 module.exports = router;
